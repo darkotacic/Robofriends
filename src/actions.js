d1849b48c75a5a2573733ab97df984ee2ce08bfa
Darko Tacic <darko.tacic@outlook.com>
2020-07-27 18:57:28 +0200

    Implemented header component

--- src/actions.js
+++ src/actions.js
@@ -15,7 +15,6 @@ export const requestRobots = () => (dispatch) => {
   fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
     })
     .catch((error) =>
