7a6c4c882cb8846a971c7894209d0b1fc9dc4f20
Darko Tacic <darko.tacic@outlook.com>
2020-08-02 15:49:20 +0200

    Implemented counter button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import CounterButton from './CounterButton';
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return false;
   }
   render() {
-    return <h1 className="f1">RoboFriends</h1>;
+    console.log('header');
+    return (
+      <div>
+        <h1 className="f1">RoboFriends</h1>
+        <CounterButton />
+      </div>
+    );
   }
 }
 
